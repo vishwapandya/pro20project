6b2fa278d3a88ed9af87e4187c2dbc52ef037a2a
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2020-12-24 13:00:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+
+//use hint 2
+
+
+//use hint 3
+
+
+function setup() {
+  createCanvas(1600, 400);
+
+
+	//use hint 4
+
+
+	//use hint 5
+
+
+	//use hint 7
+
+	car.shapeColor=color(255);
+
+
+  //use hint 6
+  	
+  	wall.shapeColor=color(80,80,80)
+}
+
+
+function draw() {
+  background(0);
+
+  //use the last hint
+
+
+
+
+  
+  
+  drawSprites();
+ 
+}
+
+
